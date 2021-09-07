53bc20e1c1326120537f0cd9ea1b16a73bd36a03
safinghoghabori <safinghoghabori65@gmail.com>
2021-09-07 22:31:00 +0530

    homepage added

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,29 @@
 import Navbar from "../Navbar/Navbar";
 import "./homepage.css";
+import headerImg from "../../images/img1.png";
 
 const HomePage = () => {
   return (
     <>
       <Navbar />
+
+      <div className="main">
+        <div className="main-content">
+          <h1>WE ARE HIRING</h1>
+          <br />
+          <p style={{ fontSize: 20 }}>
+            By adding an extended arm to your sales and marketing teams we would
+            love to strengthen your B2B marketing and sales approach. Our team
+            will help you reach larger audience of potential leads to keep you
+            ahead in the race. Feel free to get in touch with us to embark on a
+            path of success.
+          </p>
+        </div>
+        <div className="main-img">
+          <img src={headerImg} alt="" />
+        </div>
+      </div>
+
       <div className="box"></div>
     </>
   );
